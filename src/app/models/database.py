00acfa86b1de661982d9d49bffffa489dffb4c9a
Eugene Vereshchagin <veresha23@gmail.com>
2022-12-29 14:48:07 +0300

    Change DATABASE_URL in database.py Add add_items in routes.py

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -7,7 +7,7 @@ DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 HOST_ADDRESS = os.environ['HOST_ADDRESS']
 DB_NAME = os.environ['DB_NAME']
-DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{HOST_ADDRESS}/{DB_NAME}'
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{HOST_ADDRESS}/postgres'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import Depends
 from ..models.database import SessionLocal
 from sqlalchemy.orm import Session
-from ..models.schemas import Response, RequestItem
+from ..models.schemas import Response, RequestItem, ItemSchema
 
 from ..services import crud
 
@@ -17,8 +17,20 @@ def get_db():
         db.close()
 
 
+@router.post("/add_items")
+async def add_items(db: Session = Depends(get_db)):
+    item = ItemSchema()
+    for i in range(10):
+        item.title = f'{i} item'
+        item.description = f'{i} description'
+        crud.create_item(db, item=item)
+        return Response(status="Ok",
+                        code="200",
+                        message="Item created successfully").dict(exclude_none=True)
+
+
 @router.post("/create")
-async def create_book_service(request: RequestItem, db: Session = Depends(get_db)):
+async def create_item_service(request: RequestItem, db: Session = Depends(get_db)):
     crud.create_item(db, item=request.parameter)
     return Response(status="Ok",
                     code="200",
